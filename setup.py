4185a368fb85e9961fc4a65d613001424d8ec488
Haagimus <Haagimus@gmail.com>
2020-05-12 16:56:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,11 +13,12 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/BridgeCr/DOC.git',
-    python_requires='>=3.7',
+    packages=['DbObjectCreator'],
     install_requires=open('requirements.txt').read(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent'
-    ]
+    ],
+    python_requires='>=3.7'
 )
