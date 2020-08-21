825b2aa03573d24340765c6098ab641a3f25736e
Gary Haag <Haagimus@gmail.com>
2020-08-21 16:51:34 -0500

    update install_requires in setup.py

--- setup.py
+++ setup.py
@@ -2,23 +2,45 @@ from setuptools import setup, find_packages
 import DbObjectCreator.__init__ as init
 
 with open('README.md', 'r') as ld:
-    long_description = ld.read()
+	long_description = ld.read()
 
 setup(
-    name='DbObjectCreator',
-    version=init.__version__,
-    author=init.__author__,
-    author_email="haagimus@gmail.com",
-    description='A database object creator',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/haagimus/DbObjectCreator.git',
-    packages=['DbObjectCreator'],
-    install_requires=open('requirements.txt').read(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent'
-    ],
-    python_requires='>=3.7'
+		name='DbObjectCreator',
+		version=init.__version__,
+		author=init.__author__,
+		author_email="haagimus@gmail.com",
+		description='A database object creator',
+		long_description=long_description,
+		long_description_content_type='text/markdown',
+		url='https://github.com/haagimus/DbObjectCreator.git',
+		packages=['DbObjectCreator'],
+		install_requires=[
+			'bcrypt',
+			'cffi',
+			'cryptography',
+			'Cython',
+			'mysql',
+			'mysql-connector',
+			'mysqlclient',
+			'numpy',
+			'pandas',
+			'paramiko',
+			'psycopg2',
+			'pycparser',
+			'pymssql',
+			'PyMySQL',
+			'PyNaCl',
+			'python-dateutil',
+			'pytz',
+			'six',
+			'SQLAlchemy',
+			'sshtunnel',
+			'wheel'
+		],
+		classifiers=[
+			'Programming Language :: Python :: 3',
+			'License :: OSI Approved :: MIT License',
+			'Operating System :: OS Independent'
+		],
+		python_requires='>=3.7'
 )
